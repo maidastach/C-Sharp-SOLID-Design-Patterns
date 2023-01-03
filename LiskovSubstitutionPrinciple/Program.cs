ebd754a4901d7b30ffb9db931fe9d10ff084cf11
Salvatore De Rosa <Salvatore De Rosa@XPS-7590>
2023-01-03 22:04:45 +1100

    base-files-liskov-substitution-principle

--- LiskovSubstitutionPrinciple/Program.cs
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -1,4 +1,28 @@
-﻿namespace LiskovSubstitutionPrinciple
+﻿/*
+            *** LISKOV SUBSTITUTION PRINCIPLE ***
+ *  
+ * The LSP says that a child Class which inherit from a parent one
+ * should be able to implement all of the characteristics
+ * without breaking the application.
+ * 
+ * In our case CEO inherit from Employee however if we create
+ * a new CEO with an Employee interface it breaks because
+ * CEO cannot be assigned a Manager.
+ * 
+ * We do create a BaseEmployee class which includes the properties
+ * and methods in common between CEO Manager and Employee
+ * and make Employee inherit from it
+ * 
+ * We also create some interfaces for all the spares properties
+ * and methods which applies to different classes so we can implement
+ * them over the respective class
+ * 
+ * In this way we can play around with the newly created interfaces
+ * without breaking the app.
+ * 
+ */
+
+namespace LiskovSubstitutionPrinciple
 {
     class Program
     {
