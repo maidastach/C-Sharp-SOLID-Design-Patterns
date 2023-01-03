8ddd586331121e3e4da0159db9d5e6b82da6ae4e
Salvatore De Rosa <Salvatore De Rosa@XPS-7590>
2023-01-03 22:42:47 +1100

    base-files-dependency-inversion-principle

--- DependencyInversionPrinciple/Program.cs
+++ DependencyInversionPrinciple/Program.cs
@@ -1,4 +1,35 @@
-﻿namespace DependencyInversionPrinciple
+﻿/*
+ *             *** DEPENDENCY INVERSION PRINCIPLE ***
+ * 
+ * The DIP states that high level modules should not depend on low
+ * level modules but rather both should depend on abstractions and
+ * those abstractions should not depend on details.
+ * Meaning those abstractions should not have to know about how
+ * things get done, they should just say these are the things
+ * that will get done.
+ * 
+ * In this example the Main method (high level module) calls 
+ * on the Person class as well as th Chore class, it means 
+ * it depends on both those classes being there.
+ * 
+ * However the Chore class is also an high level module
+ * because it depends on the Logger and Emailer classes.
+ * 
+ * Which means every changes on the low level modules will
+ * be reflected to the high level modules and may cause 
+ * some issue. Hence we need to invert this through Interfaces.
+ * As well as we need to remove all those new Instances throughout
+ * the application, and so collecting them in a single class.
+ * 
+ * As for example i have swapped the email service with a texting
+ * service changing it in just one part, the change did not bring
+ * any issue because the app was not depending directly on that
+ * instantiation.
+ * That is the benefit of creating modular code.
+ * 
+ */
+
+namespace DependencyInversionPrinciple
 {
     class Program
     {
